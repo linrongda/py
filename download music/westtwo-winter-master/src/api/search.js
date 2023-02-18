06e0d99b3287881f69d632a3edfc2482d559910b
linrongda <1945391294@qq.com>
2023-02-18 19:09:10 +0800

    add

---
+++ download music/westtwo-winter-master/src/api/search.js
@@ -0,0 +1,14 @@
+import api from "./request.js";
+
+
+export function getSearchList(data) {
+  return api.get('search',{
+    params: data
+  })
+}
+
+export function downloadMusic(rid) {
+  return api.get(`search/download/${rid}`, {
+    responseType: "blob"
+  })
+}
---
+++ download music/westtwo-winter-master/src/router/index.js
@@ -0,0 +1,46 @@
+import {createRouter, createWebHashHistory} from "vue-router";
+
+
+import Login from "@/components/Login.vue";
+import Search from "@/components/SearchMusic.vue"
+import {ElMessage} from "element-plus";
+import { Download } from "@element-plus/icons-vue";
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/search',
+    component: Search
+  }
+]
+
+const router = createRouter({
+  routes,
+  history: createWebHashHistory()
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') {
+    // 登录或者注册才可以往下进行
+    next();
+  } else {
+    // 获取 token
+    const token = JSON.parse(localStorage.getItem('user')).token;
+    // token 不存在
+    if (token === null || token === '') {
+      ElMessage.error('您还没有登录，请先登录');
+      next('/login');
+    } else {
+      next();
+    }
+  }
+});
+
+export default router
